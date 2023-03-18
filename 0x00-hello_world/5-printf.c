1413ce32660474c7a758b276ba07261171ff04a1
Syeneo <ogbonnasyene@gmail.com>
2023-03-17 17:22:32 -0700

    C-Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string using printf function
+ * Description: using the main function
+ *
+ * This program should print the stfing:
+ * with proper grammar, but the outcome is a piece of art,.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
