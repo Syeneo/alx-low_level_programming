78ff407b012d47e1809aadae92e27c183ab06e66
Syeneo <ogbonnasyene@gmail.com>
2023-03-21 17:54:27 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,12 +9,9 @@ void print_alphabet(void)
 {
 	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		_putchar(c);
 	}
+	_putchar('\n');
 }
