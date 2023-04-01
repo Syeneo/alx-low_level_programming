fa01b7cfbc6b416b5bb96310acf8f89395442743
Syeneo <ogbonnasyene@gmail.com>
2023-04-01 06:58:51 -0700

    2nd pointers and arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,35 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * puts_half - prints the second half of a string
- * @str: string to be printed
+ * puts_half - prints a string
+ * @str: string to print
  *
- * Return: void
+ * Return: nothing
  */
+
 void puts_half(char *str)
 {
-	int i, mid;
-	int a = 0;
+	int i, len = _strlen(str);
 
-	while (str[a] != '\0')
-	{
-		a++;
-	}
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
+	putchar(10);
+}
 
-	if (a % 2 == 0)
-	{
-		for (i = a / 2; i < a; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		mid = (a - 1) / 2;
-		for (i = mid + 1; i < a; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
